f1ac3c4bbac5cc1aadd4a25c504ff0cbf33cebf5
Kostia Savchenko <k.savchenko.dev@gmail.com>
2025-02-25 17:31:24 +0300

    добавил обработку ошибок при получении данных из БД в функции getChats

--- src/services/getChats.ts
+++ src/services/getChats.ts
@@ -1,9 +1,14 @@
 import { db } from "./connectDataBase.js";
 
 export async function getChats() {
-  const result = await db.query(`
-    SELECT name, TRIM(link) AS link, note FROM events
-  `);
-  console.log("🔍 Данные из БД:", result.rows); 
-  return result.rows;
+  try {
+    const result = await db.query(`
+      SELECT name, TRIM(link) AS link, note FROM events
+    `);
+    console.log("🔍 Данные из БД:", result.rows); 
+    return result.rows;
+  } catch (error) {
+    console.error("❌ Ошибка при получении данных из БД:", error);
+    throw error;
+  }
 }
