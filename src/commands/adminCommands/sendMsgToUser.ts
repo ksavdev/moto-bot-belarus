930304fe8a9c3a1cdbf245e8d6f020f1c2a19e66
Kostia Savchenko <k.savchenko.dev@gmail.com>
2025-02-26 12:34:54 +0300

    исправил опечатку в названии команды отправки сообщения пользователю для корректной работы

--- src/commands/adminCommands/sendMsgToUser.ts
+++ src/commands/adminCommands/sendMsgToUser.ts
@@ -3,7 +3,7 @@ import { MyContext } from "../../bot.js";
 
 export const sendMsgToUser = (bot: Bot<MyContext>) => {
     bot.command("sendmsg", async (ctx) => {
-        if (ctx.config.isDeveloper) await ctx.conversation.enter("sendMsgToUserConversaation");
+        if (ctx.config.isDeveloper) await ctx.conversation.enter("sendMsgToUserConversation");
         else await ctx.reply("Ты не имеешь доступа к этой команде.");
     });
 };
