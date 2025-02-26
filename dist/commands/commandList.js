2ac3b941543ca57ee1ad9f9a3bb4834453824442
Kostia Savchenko <k.savchenko.dev@gmail.com>
2025-02-26 09:18:16 +0300

    обновил приветственное сообщение бота и добавил новые команды в меню

--- dist/commands/commandList.js
+++ dist/commands/commandList.js
@@ -1,7 +1,12 @@
 export const setupBotCommands = (bot) => {
     bot.api.setMyCommands([
-        { command: 'start', description: "Запуск бота" },
+        { command: 'start', description: "Перезапустить бота" },
+        { command: 'startmenu', description: "Главное меню" },
+        { command: 'newbie', description: "Меню новичка" },
+        { command: 'senior', description: "Меню для опытных" },
+        { command: 'guide', description: "Меню гайдов" },
         { command: 'help', description: "Помощь" },
         { command: 'contact', description: "Написать разработчику" },
+        { command: 'ad', description: "Предложения по рекламе" },
     ]);
 };
--- src/commands/commandList.ts
+++ src/commands/commandList.ts
@@ -3,9 +3,13 @@ import { MyContext } from '../bot.js';
 
 export const setupBotCommands = (bot: Bot<MyContext>) => {
     bot.api.setMyCommands([
-        { command: 'start', description: "Запуск бота" },
+        { command: 'start', description: "Перезапустить бота" },
+        { command: 'startmenu', description: "Главное меню" },
+        { command: 'newbie', description: "Меню новичка" },
+        { command: 'senior', description: "Меню для опытных" },
+        { command: 'guide', description: "Меню гайдов" },
         { command: 'help', description: "Помощь" },
         { command: 'contact', description: "Написать разработчику" },
-        
+        { command: 'ad', description: "Предложения по рекламе" },
     ]);
 };
