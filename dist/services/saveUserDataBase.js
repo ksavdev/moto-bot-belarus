66ed09167640d7753aa51bb894ef8cf9f012ffde
Kostia Savchenko <k.savchenko.dev@gmail.com>
2025-02-27 09:58:37 +0300

    fix: update database query to use telegramID instead of id for user insertion

--- dist/services/saveUserDataBase.js
+++ dist/services/saveUserDataBase.js
@@ -3,8 +3,9 @@ export async function saveUserInDataBase(name, surname, id, username) {
     const client = await openDB();
     try {
         await client.query(`
-            INSERT INTO users (name, surname, id, username)
-            `);
+            INSERT INTO users (name, surname, telegramID, username)
+            VALUES ($1, $2, $3, $4)
+        `, [name, surname, id, username]);
     }
     catch (error) {
         console.error("❌ Ошибка при сохранении данных в БД:", error);
--- src/services/saveUserDataBase.ts
+++ src/services/saveUserDataBase.ts
@@ -4,8 +4,9 @@ export async function saveUserInDataBase(name: string, surname: string, id: numb
     const client = await openDB();
     try {
         await client.query(`
-            INSERT INTO users (name, surname, id, username)
-            `);
+            INSERT INTO users (name, surname, telegramID, username)
+            VALUES ($1, $2, $3, $4)
+        `, [name, surname, id, username]);
     } catch (error) {
         console.error("❌ Ошибка при сохранении данных в БД:", error);
         throw error;
