83454c88806ce3d195d1b55caa0f807eecae14fb
Kostia Savchenko <k.savchenko.dev@gmail.com>
2025-02-27 14:56:30 +0300

    fix: correct SQL query syntax in getMotoConsumablesShops function

--- dist/services/getMotoConsumablesShops.js
+++ dist/services/getMotoConsumablesShops.js
@@ -3,7 +3,7 @@ export async function getMotoConsumablesShops() {
     const client = await openDB();
     try {
         const result = await client.query(`
-      SELECT name, address, TRIM(link) AS link, note moto_consumables_shops
+      SELECT name, address, TRIM(link) AS link, note FROM moto_consumables_shops
     `);
         console.log("🔍 Данные из БД:", result.rows); // Логируем полученные данные
         return result.rows;
